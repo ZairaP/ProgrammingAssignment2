788ecee90aa6c57e005486f8e2c36ba6cffd64fe
Zaira Pabon Martínez <zairap75@gmail.com>
2014-04-27 12:24:02 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,25 @@
-makeCacheMatrix <- function(x = matrix()) { ##creation of special matrix object
-	 inv <- NULL   ##inicialization of inverse matrix
-	set <- function(y) {  ##function of set matrix's values.##if matrix changed, then its inverse is initialized again 
-		x <<- y
+makeCacheMatrix <- function(mat = matrix()) { ##creation of smat object
+	 inv <- NULL   ##inicialization of inv mat.
+	set <- function(y) {  ##function of set matrix's values.##if matrix changed, then  inverse is initialized a new
+		mat <<- y
 		inv <<- NULL   
 	}
-	 get <- function() x	 ##return matrix
-	setinv <- function(inverse) inv <<- inverse  ##set matrix inverse
+	 get <- function() mat	 ##return mat
+	setinv <- function(inverse) inv <<- inverse  ##set inv mat.
 	getinv <- function() inv ##return inverse of matrix, if matrix inverse hasn't calculated yet then return null
        	list(set = set,get = get, ##list of functions
 		setinv = setinv,
 		getinv = getinv)
 }
 
-cacheSolve <- function(x) { ##function that calcule and return the inverse of special matrix 'x'
-	inv <- x$getinv() ## first get the inverse of matrix
-        if(!is.null(inv)) { ## if inverse has already calculated then return it
+cacheSolve <- function(mat) { ## this function calcule and return the inverse of special matrix 'mat'
+	inv <- mat$getinv() ## first get the inv matrix.
+        if(!is.null(inv)) { ## if inverse has already calculated then return it.
                 message("getting cached data")
                 return(inv)
         }
-         data <- x$get() ##if not, then get data of x 
+         data <- mat$get() ##if not, then get data of mat
          inv <- solve(data) ##calcule inverse of adquired data
-        x$setinv(inv) ##set the inverse of object x
+        mat$setinv(inv) ##set the inverse of object mat
         inv	##return the inverse
 }
